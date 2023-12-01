ddf464043190140975ffa73da09fbc2beb378be5
Pradeep <mpradip17@users.noreply.github.com>
2023-12-01 17:52:07 +0530

    Delete app directory

--- app/main/java/com/talentwood/model/CustomViewPager2.kt
+++
@@ -1,9 +0,0 @@
-package com.talentwood.model
-
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-
-data class CustomViewPager2(
-    val fragment: Fragment,
-    val bundle: Bundle? = null,
-)
