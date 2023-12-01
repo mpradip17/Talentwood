4c7e958a4844c6c0f6f313c95b57320b8843c854
Pradeep <mpradip17@users.noreply.github.com>
2023-12-01 17:42:48 +0530

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,66 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.1.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    /*id("kotlin-parcelize")
+    id("org.jetbrains.kotlin.plugin.serialization")*/
+}
+
+android {
+    namespace = "com.talentwood"
+    compileSdk = 34
+    buildFeatures {
+        viewBinding = true
+    }
+    defaultConfig {
+        applicationId = "com.talentwood"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+}
+
+dependencies {
+
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("com.google.android.material:material:1.10.0")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.4")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.4")
+
+    implementation("com.airbnb.android:lottie:6.1.0")
+    implementation("com.intuit.sdp:sdp-android:1.1.0")
+    implementation("com.intuit.ssp:ssp-android:1.1.0")
+    implementation("com.github.appsfeature:otp-view:1.0")
+
+    implementation("androidx.preference:preference:1.2.1")
+
+    implementation("com.google.code.gson:gson:2.10.1")
+
+
+    // implementation("com.github.skydoves:powerspinner:1.2.7")
+
 }
